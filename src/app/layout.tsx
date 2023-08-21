54b8d5db2d3f01c9aa5b46d8df6a204ea1f85a0c
Usama Shahzad <usamasardar019@gmail.com>
2023-08-21 12:27:43 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer />
+      </body>
     </html>
   )
 }
